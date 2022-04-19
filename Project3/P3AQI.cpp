4c7538e1c48fd372bf1d440170db9cb7457f31c8
Max <phillipsmax14@outlook.com>
2022-04-19 13:47:10 -0400

    second

--- Project3/P3AQI.cpp
+++ Project3/P3AQI.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
-//#include<ifstream>
-#include <map>
-#include <string>
-#include <utility>
 using namespace std;
-int main () {
+#include<iostream>
+#include<fstream>
+#include <map>
+#include<string>
+#include <utility>
+
+int main ()
+ {
+    vector<vector<std::string>> csv;
+    vector<string> data;
     string name = "";
     string county = "";
     int year;
     int medAQI;
+    fstream file(Project3.csv,ios::in);
+    if(Project3.csv.is_open())
+    {
+
+
+    }
     std::pair<string,string> pair1;
     std::pair<int,int> pair2;
     //input
@@ -17,13 +27,13 @@ int main () {
     //map input
     map<pair<string,string>,pair<int,int>> pollutionMap;
     //map<string,int>::iterator iter;
-    for(for i = 4; i < input.size(); i+4)
+    for(for i = 4; i < csv.size(); i+4)
     {
-        name = input.at(i);
-        county = input.at(i+1);
+        name = csv.at(i);
+        county = csv.at(i+1);
         pair1 = make_pair(name,county);
-        year = input.at(i+2);
-        medAQI = input.at(i+3);
+        year = csv.at(i+2);
+        medAQI = csv.at(i+3);
         pair2 = make_pair(year,medAQI);
         pollutionMap.insert(pair1,pair2);
     }
